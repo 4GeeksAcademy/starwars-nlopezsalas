8c6a978ca60bee2b96bba76c5a0227ed953a46f7
Natalia <nlopezsalas@gmail.com>
2024-02-27 20:40:50 +0000

    1rst version

--- src/js/component/planetCard.js
+++ src/js/component/planetCard.js
@@ -35,9 +35,14 @@ function PlanetCard(props) {
 
 	}, []);
 
+	
 	return (
-		<Card className="card" style={{ width: '18rem' }} key={props.key}>
-			<Card.Img variant="top" src="holder.js/100px180" />
+		<Card className="card" style={{ width: '18rem' }} key={props.uid}>
+			{(props.name === "Tatooine") 
+			?
+			<Card.Img variant="top" src="https://starwars-visualguide.com/assets/img/planets/2.jpg" />
+			:
+			<Card.Img variant="top" src={`https://starwars-visualguide.com/assets/img/planets/${props.uid}.jpg`} />}
 			<Card.Body>
 				<Card.Title>{props.name}</Card.Title>
 				<Card.Text className="no-wrap">
